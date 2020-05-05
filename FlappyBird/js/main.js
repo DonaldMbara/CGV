fb2c24d0bdfa20e0270dc027e6c0746eae7b075d
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-05-05 14:16:52 +0200

    added bird model with animation

--- FlappyBird/js/main.js
+++ FlappyBird/js/main.js
@@ -1,21 +1,26 @@
 /*
  * Group : Boolean Autocrats
  */
-window.addEventListener('load', init, false);
+window.addEventListener('load', init, false); //load our init() function
+
+
 // Standard three.js requirements.
 var renderer;
 var camera;
 var scene = new THREE.Scene();
 
 
-//other variables
+//Model  variables
+var mixers = [];
+const clock = new THREE.Clock();
 const birdModel_PATH = 'model/Stork.glb';
-var loader = new THREE.GLTFLoader(); // constructor for loading model
+var birdModel;
+var loader;
 
 var plane;
 
 
-//camera variables
+//camera variables and parameters variables
 var fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH,
     renderer, container;
 
@@ -28,9 +33,10 @@ var sea;
 var sky;
 var cloud
 
-const mixers = [];
 
 
+
+//----------------------------Main function for initialising----------------------------
 
 
 function init() {
@@ -51,9 +57,16 @@ function init() {
     //document.addEventListener('mousemove', handleMouseMove, false);
 
     //render the scene on each frame
+    renderer.setAnimationLoop(() => {
+
+        update();
+
+    });
     render();
 
 }
+
+//-------------------------The Scene ----------------------------------------------
 
 createScene = function() {
     //height and width of the screen
@@ -96,7 +109,10 @@ createScene = function() {
 
 }
 
-var hemisphereLight, shadowLight;
+
+//------------------------Lights---------------------------------------------
+
+
 
 function createLights() {
     //hemisphere light for gradient colored light; 
@@ -128,74 +144,66 @@ function createLights() {
 }
 
 
-function Sea() {
-
-    // create the geometry (shape) of the cylinder;
-    // the parameters are: 
-    // radius top, radius bottom, height, number of segments on the radius, number of segments vertically
-    var geometry = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
-
-    // rotate the geometry on the x axis
-    geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
-
-    // create the material 
-    var material = new THREE.MeshPhongMaterial({
-        color: "rgb(0,119,190)",
-        transparent: true,
-        opacity: .6,
-        flatShading: THREE.FlatShading,
-    });
-
-
-    this.mesh = new THREE.Mesh(geometry, material);
-
-    // Allow the sea to receive shadows
-    this.mesh.receiveShadow = true;
-}
 
 
 
-function createSea() {
-    sea = new Sea();
-
-    // bottom of the scene
-    sea.mesh.position.y = -600;
-
-    // add to the scene
-    scene.add(sea.mesh);
-}
 
 
-
+//-------------------------Our Hero(The bird)-------------------------------------------
 
 createBird = function() {
 
+    loader = new THREE.GLTFLoader(); // constructor for loading model
 
-    loader.load(
-        birdModel_PATH,
-        function(gltf) {
-            // ]
-            model = gltf.scene;
-            let fileAnimations = gltf.animations;
-            model.traverse(o => {
-                if (o.isMesh) {
-                    o.castShadow = true;
-                    o.receiveShadow = true;
-                }
-            });
-            // Set the models initial scale
-            model.position.y = 100;
-            // Set the models initial scale
-            model.scale.set(7, 7, 7);
 
-            scene.add(model);
-        },
-        undefined, // We don't need this function
-        function(error) {
-            console.error(error);
-        }
-    );
+    const onLoad = (gltf, position) => {
+
+        birdModel = gltf.scene.children[0];
+        birdModel.position.copy(position);
+
+        const animation = gltf.animations[0];
+
+        const mixer = new THREE.AnimationMixer(birdModel);
+        mixers.push(mixer);
+        birdModel.scale.set(7, 7, 7); //increase size of the bird by a factor of 7
+
+        birdModel.rotation.z = -1.5; //rotating the bird
+        birdModel.position.set(-50, 130, -20); // position the bird
+
+
+
+        const action = mixer.clipAction(animation); //bird animation
+        action.play(); //animation ignited
+
+        //setting shadow of the bird
+        birdModel.traverse(o => {
+            if (o.isMesh) {
+                o.castShadow = true;
+                o.receiveShadow = true;
+            }
+        });
+
+        scene.add(birdModel);
+
+    };
+
+    // the loader will report the loading progress to this function
+    const onProgress = () => {};
+
+    // the loader will send any error messages to this function, and we'll log
+    // them to to console
+    const onError = (errorMessage) => { console.log(errorMessage); };
+
+    // load the model. The model is loaded asynchronously
+    const bird = new THREE.Vector3(0, 0, 0);
+    loader.load(birdModel_PATH, gltf => onLoad(gltf, bird), onProgress, onError);
+
+
+
+
 }
+
+//-----------------------Clouds-------------------------------------------------
 
 Cloud = function() {
     // empty container that will hold the different parts of the cloud
@@ -237,6 +245,8 @@ Cloud = function() {
 }
 
 
+//-----------------------------------The Sky-----------------------------------------------------
+
 Sky = function() {
     // Create an empty container
     this.mesh = new THREE.Object3D();
@@ -250,7 +260,7 @@ Sky = function() {
 
     // create the clouds
     for (var i = 0; i < this.nClouds; i++) {
-        var c = new Cloud();
+        var cloud = new Cloud();
 
         // set the rotation and the position of each cloud;
         var a = stepAngle * i; // this is the final angle of the cloud
@@ -258,26 +268,25 @@ Sky = function() {
 
 
         //  converting polar coordinates (angle, distance) into Cartesian coordinates (x, y)
-        c.mesh.position.y = Math.sin(a) * h;
-        c.mesh.position.x = Math.cos(a) * h;
+        cloud.mesh.position.y = Math.sin(a) * h;
+        cloud.mesh.position.x = Math.cos(a) * h;
 
         // rotating the cloud according to its position
-        c.mesh.rotation.z = a + Math.PI / 2;
+        cloud.mesh.rotation.z = a + Math.PI / 2;
 
         // for a better result, we position the clouds 
         // at random depths inside of the scene
-        c.mesh.position.z = -400 - Math.random() * 400;
+        cloud.mesh.position.z = -400 - Math.random() * 400;
 
         // we also set a random scale for each cloud
         var s = 1 + Math.random() * 2;
-        c.mesh.scale.set(s, s, s);
+        cloud.mesh.scale.set(s, s, s);
 
-        this.mesh.add(c.mesh);
+        this.mesh.add(cloud.mesh);
     }
 }
 
-// Now we instantiate the sky and push its center a bit
-// towards the bottom of the screen
+
 
 
 
@@ -287,6 +296,122 @@ function createSky() {
     scene.add(sky.mesh);
 }
 
+
+//-------------------------The Sea ------------------------------------------------------------
+
+function Sea() {
+
+    // create the geometry (shape) of the cylinder;
+    // the parameters are: 
+    // radius top, radius bottom, height, number of segments on the radius, number of segments vertically
+    var geometry = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+
+    // rotate the geometry on the x axis
+    geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+    // create the material 
+    var material = new THREE.MeshPhongMaterial({
+        color: "rgb(0,119,190)",
+        transparent: true,
+        opacity: .6,
+        flatShading: THREE.FlatShading,
+    });
+
+
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    // Allow the sea to receive shadows
+    this.mesh.receiveShadow = true;
+}
+
+
+
+function createSea() {
+    sea = new Sea();
+
+    // bottom of the scene
+    sea.mesh.position.y = -600;
+
+    // add to the scene
+    scene.add(sea.mesh);
+}
+
+
+
+//-------------------------The Sea Waves ------------------------------------------------------
+
+Sea = function() {
+    var geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+    geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+    // important: by merging vertices we ensure the continuity of the waves
+    geom.mergeVertices();
+
+    // get the vertices
+    var l = geom.vertices.length;
+
+    // create an array to store new data associated to each vertex
+    this.waves = [];
+
+    for (var i = 0; i < l; i++) {
+        // get each vertex
+        var v = geom.vertices[i];
+
+        // store some data associated to it
+        this.waves.push({
+            y: v.y,
+            x: v.x,
+            z: v.z,
+            // a random angle
+            ang: Math.random() * Math.PI * 2,
+            // a random distance
+            amp: 5 + Math.random() * 15,
+            // a random speed between 0.016 and 0.048 radians / frame
+            speed: 0.016 + Math.random() * 0.032
+        });
+    };
+    var mat = new THREE.MeshPhongMaterial({
+        color: "rgb(0,119,190)",
+        transparent: true,
+        opacity: .8,
+        shading: THREE.FlatShading,
+    });
+
+    this.mesh = new THREE.Mesh(geom, mat);
+    this.mesh.receiveShadow = true;
+
+}
+
+
+Sea.prototype.moveWaves = function() {
+
+    // get the vertices
+    var verts = this.mesh.geometry.vertices;
+    var l = verts.length;
+
+    for (var i = 0; i < l; i++) {
+        var v = verts[i];
+
+        // get the data associated to it
+        var vprops = this.waves[i];
+
+        // update the position of the vertex
+        v.x = vprops.x + Math.cos(vprops.ang) * vprops.amp;
+        v.y = vprops.y + Math.sin(vprops.ang) * vprops.amp;
+
+        // increment the angle for the next frame
+        vprops.ang += vprops.speed;
+
+    }
+
+
+    this.mesh.geometry.verticesNeedUpdate = true;
+
+    sea.mesh.rotation.z += .005;
+}
+
+
+//----------------------------window resizing----------------------------------
 
 function windowResize() {
     // update height and width of the renderer and the camera
@@ -298,11 +423,49 @@ function windowResize() {
 }
 
 
+
+//---------------bird animation update -------------------------------
+
+function update() {
+
+    const delta = clock.getDelta();
+
+    for (const mixer of mixers) {
+
+        mixer.update(delta);
+
+    }
+
+}
+
+
+
+
+
+// waveSound = function() {
+
+//     //add it to the camera
+
+
+//     // load a sound and set it as the Audio object's buffer
+//     var audioLoader = new THREE.AudioLoader();
+//     audioLoader.load('sounds/ambient.ogg', function(buffer) {
+//         sound.setBuffer(buffer);
+//         sound.setLoop(true);
+//         sound.setVolume(0.5);
+//         sound.play();
+//     });
+// }
+
+//----------------------Rendering-----------------------------------------------
+
+
 function render() {
 
     // Rotate the sea and the sky
-    sea.mesh.rotation.z += .005;
+    sea.mesh.rotation.z += .0015;
     sky.mesh.rotation.z += .01;
+    sea.moveWaves(); //wave 
     renderer.render(scene, camera);
     // call the loop function again
     requestAnimationFrame(render);
